0e5ebd22fd13e58f08661021730c5dd053e03a06
Vorobey Aleksandr Vladimirovich <a.v.vorobey@tinkoff.ru>
2018-10-24 19:42:42 +0300

    changed mfn argument type

--- tests/mfn.spec.js
+++ tests/mfn.spec.js
@@ -1,109 +1,142 @@
 const mfn = require('../lib/storage').__mfn
 
 it('Generates same names for same request', () => {
-  const name1 = mfn('http://example.com')
-  const name2 = mfn('http://example.com')
+  const name1 = mfn({ url: 'http://example.com' })
+  const name2 = mfn({ url: 'http://example.com' })
 
   expect(name1).toBe('get-d3c8eae0')
   expect(name2).toBe(name1)
 })
 
 it('skipQueryParams does not affects output name', () => {
-  const m = 'GET'
-  const p = ''
   const skipQueryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&x=y', m, null, p, [], skipQueryParams)
-  const name2 = mfn('http://example.com?x=y&foo=bazzzzz', m, null, p, [], skipQueryParams)
+  const name1 = mfn({ url: 'http://example.com?foo=bar&x=y', skipQueryParams })
+  const name2 = mfn({ url: 'http://example.com?x=y&foo=bazzzzz', skipQueryParams })
 
   expect(name1).toBe(name2)
 })
 
 it('queryParams does not affect output name', () => {
-  const m = 'GET'
-  const p = ''
   const queryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&y=x', m, null, p, queryParams)
-  const name2 = mfn('http://example.com?foo=bar', m, null, p, queryParams)
+  const name1 = mfn({ url: 'http://example.com?foo=bar&y=x', queryParams })
+  const name2 = mfn({ url: 'http://example.com?foo=bar', queryParams })
 
   expect(name1).toBe(name2)
 })
 
 it('unnecessary params bigger than 1 does not affect output name', () => {
-  const m = 'GET'
-  const p = ''
   const queryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&x=y&y=x', m, null, p, queryParams)
-  const name2 = mfn('http://example.com?foo=bar', m, null, p, queryParams)
+  const name1 = mfn({ url: 'http://example.com?foo=bar&x=y&y=x', queryParams })
+  const name2 = mfn({ url: 'http://example.com?foo=bar', queryParams })
+
   expect(name1).toBe(name2)
 })
 
 it('queryParams > 1 does not affect output name', () => {
-  const m = 'GET'
-  const p = ''
   const queryParams = ['foo', 'trois']
-  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, null, p, queryParams)
-  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, null, p, queryParams)
+  const name1 = mfn({ url: 'http://example.com?foo=bar&trois=quatre&x=y', queryParams })
+  const name2 = mfn({ url: 'http://example.com?trois=quatre&foo=bar', queryParams })
+
   expect(name1).toBe(name2)
 })
 
 it('skip params from queryParams does not affect output name', () => {
-  const m = 'GET'
-  const p = ''
   const queryParams = ['foo', 'trois']
   const skipQueryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, null, p, queryParams, skipQueryParams)
-  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, null, p, queryParams, skipQueryParams)
+  const name1 = mfn({ url: 'http://example.com?foo=bar&trois=quatre&x=y', queryParams, skipQueryParams })
+  const name2 = mfn({ url: 'http://example.com?trois=quatre&foo=bar', queryParams, skipQueryParams })
+
   expect(name1).toBe(name2)
 })
 
 it('Skipped post body params for request with content-type="application/json" does not affects output name', () => {
-  const m = 'POST'
-  const postBody1 = JSON.stringify({ foo: 'bar', x: 2 })
-  const postBody2 = JSON.stringify({ foo: 'bazzzz', x: 2 })
+  const method = 'POST'
   const skipPostParams = ['foo']
   const headers = {"content-type": "application/json"}
-  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
-  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+  const name1 = mfn({ 
+    url: 'http://example.com', 
+    method, 
+    headers, 
+    postData: JSON.stringify({ foo: 'bar', x: 2 }), 
+    skipPostParams
+  })
+  const name2 = mfn({
+    url: 'http://example.com', 
+    method, 
+    headers, 
+    postData: JSON.stringify({ foo: 'bazzzz', x: 2 }), 
+    skipPostParams
+  })
 
   expect(name1).toBe(name2)
 })
 
 it('Skipped post body params for request with content-type="application/x-www-form-urlencoded" does not affects output name', () => {
-  const m = 'POST'
-  const postBody1 = "foo=bar&x=2"
-  const postBody2 = "foo=bazzzz&x=2"
   const skipPostParams = ['foo']
   const headers = {"content-type": "application/x-www-form-urlencoded"}
-  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
-  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+
+  const name1 = mfn({
+    url: 'http://example.com', 
+    method: 'POST', 
+    headers, 
+    postData: "foo=bar&x=2", 
+    skipPostParams
+  })
+  const name2 = mfn({
+    url: 'http://example.com', 
+    method: 'POST', 
+    headers, 
+    postData: "foo=bazzzz&x=2", 
+    skipPostParams
+  })
 
   expect(name1).toBe(name2)
 })
 
 it('Skipped post body params for request without content-type affects output name', () => {
-  const m = 'POST'
-  const postBody1 = "foo=bar&x=2"
-  const postBody2 = "foo=bazzzz&x=2"
+  const method = 'POST'
   const skipPostParams = ['foo']
   const headers = {}
-  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
-  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+  const name1 = mfn({
+    url: 'http://example.com', 
+    method, 
+    headers, 
+    postData: "foo=bar&x=2",
+    skipPostParams
+  })
+  const name2 = mfn({
+    url: 'http://example.com', 
+    method, 
+    headers,
+    postData: "foo=bazzzz&x=2", 
+    skipPostParams
+  })
 
-  expect(name1).not.toBe(name2)
+  expect(name1).toBe(name2)
 })
 
 it('Skipped post body params for request with not supported content-type affects output name', () => {
-  const m = 'POST'
-  const postBody1 = "foo=bar&x=2"
-  const postBody2 = "foo=bazzzz&x=2"
+  const method = 'POST'
   const skipPostParams = ['foo']
   const headers =  {"content-type": "multipart/form-data"}
-  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
-  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+  const name1 = mfn({
+    url: 'http://example.com', 
+    method, 
+    headers, 
+    postData: "foo=bar&x=2",
+    skipPostParams
+  })
+  const name2 = mfn({
+    url: 'http://example.com', 
+    method, 
+    headers, 
+    postData: "foo=bazzzz&x=2", 
+    skipPostParams
+  })
 
-  expect(name1).not.toBe(name2)
+  expect(name1).toBe(name2)
 })
 
 it('Non-json post body does not throws an error', () => {
-  mfn('http://example.com', 'GET', null, 'post_body')
+  mfn({ url: 'http://example.com', postData: 'post_body' })
 })
