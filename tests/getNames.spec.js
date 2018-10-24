5d80e5a93b160c1cf02dc202e61acaf99612a265
Vorobey Aleksandr Vladimirovich <a.v.vorobey@tinkoff.ru>
2018-10-24 13:03:42 +0300

    added correct parameter skipping for content-type=x-www-form-urlencoded

--- tests/getNames.spec.js
+++ tests/getNames.spec.js
@@ -6,6 +6,7 @@ it('Generates filename for domain without slash', () => {
   const names = getNames(
     'http://example.com',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
@@ -18,6 +19,7 @@ it('Generates filename for domain with slash', () => {
   const names = getNames(
     'http://example.com/',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
@@ -30,6 +32,7 @@ it('Generates filename for domain with slugs', () => {
   const names = getNames(
     'http://example.com/foo/bar',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
@@ -42,6 +45,7 @@ it('Generates filename for domain with slugs and trailing slash', () => {
   const names = getNames(
     'http://example.com/foo/bar/',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
@@ -54,12 +58,14 @@ it('Generates different filenames for different query params', () => {
   const names1 = getNames(
     'http://example.com/foo/bar/?foo=bar',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
   const names2 = getNames(
     'http://example.com/foo/bar/?baz=1',
     'GET',
+    null,
     '',
     '/diokuz/dir',
   )
@@ -72,6 +78,7 @@ it('Generates same filenames for different skipped query params', () => {
   const names1 = getNames(
     'http://example.com/foo/bar/?foo=bar&x=y',
     'GET',
+    null,
     '',
     '/diokuz/dir',
     [],
@@ -80,6 +87,7 @@ it('Generates same filenames for different skipped query params', () => {
   const names2 = getNames(
     'http://example.com/foo/bar/?foo=bar&x=y&random=123',
     'GET',
+    null,
     '',
     '/diokuz/dir',
     [],
@@ -94,6 +102,7 @@ it('Generates same filenames for different order of query params', () => {
   const names1 = getNames(
     'http://example.com/foo/bar/?foo=bar&x=y',
     'GET',
+    null,
     '',
     '/diokuz/dir',
     [],
@@ -102,6 +111,7 @@ it('Generates same filenames for different order of query params', () => {
   const names2 = getNames(
     'http://example.com/foo/bar/?x=y&foo=bar',
     'GET',
+    null,
     '',
     '/diokuz/dir'
   )
@@ -110,20 +120,52 @@ it('Generates same filenames for different order of query params', () => {
   expect(names1.absFileName).toBe(names2.absFileName)
 })
 
-it('Generates different filenames for different post bodies', () => {
-  const names1 = getNames('http://example.com', 'POST', 'post_body_1', '/diokuz/dir')
-  const names2 = getNames('http://example.com', 'POST', 'post_body_2', '/diokuz/dir')
+it('Generates different filenames for different post bodies without content-type', () => {
+  const names1 = getNames('http://example.com', 'POST',  null, 'post_body_1', '/diokuz/dir')
+  const names2 = getNames('http://example.com', 'POST',  null, 'post_body_2', '/diokuz/dir')
 
   expect(names1.absFileName).toBe('/diokuz/dir/example.com/post-28450036')
   expect(names1.absFileName).not.toBe(names2.absFileName)
 })
 
-it('Generates same filenames for different skipped post bodies', () => {
+it('Generates different filenames for different FromData post bodies', () => {
+  const headers = {"content-type": "application/x-www-form-urlencoded"}
+
+  const names1 = getNames('http://example.com', 'POST',  headers, "foo=bar&x=2", '/diokuz/dir')
+  const names2 = getNames('http://example.com', 'POST',  headers, "foo=bazzzz&x=2", '/diokuz/dir')
+
+  expect(names1.absFileName).toBe('/diokuz/dir/example.com/post-2e223b54')
+  expect(names1.absFileName).not.toBe(names2.absFileName)
+})
+
+it('Generates different filenames for different JSON post bodies', () => {
+  const b1 = JSON.stringify({ id: 1, randomId: 2, timestamp: 123 })
+  const b2 = JSON.stringify({ id: 1, randomId: 3, timestamp: 321 })
+  const names1 = getNames('http://example.com', 'POST',  null, b1, '/diokuz/dir')
+  const names2 = getNames('http://example.com', 'POST',  null, b2, '/diokuz/dir')
+
+  expect(names1.absFileName).toBe('/diokuz/dir/example.com/post-2edaa4dd')
+  expect(names1.absFileName).not.toBe(names2.absFileName)
+})
+
+it('Generates same filenames for different skipped FormData post bodies', () => {
+  const spp = ['foo']
+
+  const headers = {"content-type": "application/x-www-form-urlencoded"}
+  const names1 = getNames('http://example.com', 'POST', headers, "foo=bar&x=2", '/diokuz/dir', [], [], spp)
+  const names2 = getNames('http://example.com', 'POST', headers, "foo=bazzzz&x=2", '/diokuz/dir', [], [], spp)
+
+  expect(names1.absFileName).toBe('/diokuz/dir/example.com/post-faa5fbbe')
+  expect(names1.absFileName).toBe(names2.absFileName)
+})
+
+it('Generates same filenames for different skipped JSON post bodies', () => {
   const spp = ['randomId', 'timestamp']
   const b1 = JSON.stringify({ id: 1, randomId: 2, timestamp: 123 })
   const b2 = JSON.stringify({ id: 1, randomId: 3, timestamp: 321 })
-  const names1 = getNames('http://example.com', 'POST', b1, '/diokuz/dir', [], [], spp)
-  const names2 = getNames('http://example.com', 'POST', b2, '/diokuz/dir', [], [], spp)
+  const headers = {"content-type": "application/json"}
+  const names1 = getNames('http://example.com', 'POST', headers, b1, '/diokuz/dir', [], [], spp)
+  const names2 = getNames('http://example.com', 'POST', headers, b2, '/diokuz/dir', [], [], spp)
 
   expect(names1.absFileName).toBe('/diokuz/dir/example.com/post-5b9e6a38')
   expect(names1.absFileName).toBe(names2.absFileName)
--- tests/mfn.spec.js
+++ tests/mfn.spec.js
@@ -12,8 +12,8 @@ it('skipQueryParams does not affects output name', () => {
   const m = 'GET'
   const p = ''
   const skipQueryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&x=y', m, p, [], skipQueryParams)
-  const name2 = mfn('http://example.com?x=y&foo=bazzzzz', m, p, [], skipQueryParams)
+  const name1 = mfn('http://example.com?foo=bar&x=y', m, null, p, [], skipQueryParams)
+  const name2 = mfn('http://example.com?x=y&foo=bazzzzz', m, null, p, [], skipQueryParams)
 
   expect(name1).toBe(name2)
 })
@@ -22,9 +22,9 @@ it('queryParams does not affect output name', () => {
   const m = 'GET'
   const p = ''
   const queryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&y=x', m, p, queryParams)
-  console.log('second')
-  const name2 = mfn('http://example.com?foo=bar', m, p, queryParams)
+  const name1 = mfn('http://example.com?foo=bar&y=x', m, null, p, queryParams)
+  const name2 = mfn('http://example.com?foo=bar', m, null, p, queryParams)
+
   expect(name1).toBe(name2)
 })
 
@@ -32,8 +32,8 @@ it('unnecessary params bigger than 1 does not affect output name', () => {
   const m = 'GET'
   const p = ''
   const queryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&x=y&y=x', m, p, queryParams)
-  const name2 = mfn('http://example.com?foo=bar', m, p, queryParams)
+  const name1 = mfn('http://example.com?foo=bar&x=y&y=x', m, null, p, queryParams)
+  const name2 = mfn('http://example.com?foo=bar', m, null, p, queryParams)
   expect(name1).toBe(name2)
 })
 
@@ -41,8 +41,8 @@ it('queryParams > 1 does not affect output name', () => {
   const m = 'GET'
   const p = ''
   const queryParams = ['foo', 'trois']
-  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, p, queryParams)
-  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, p, queryParams)
+  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, null, p, queryParams)
+  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, null, p, queryParams)
   expect(name1).toBe(name2)
 })
 
@@ -51,22 +51,59 @@ it('skip params from queryParams does not affect output name', () => {
   const p = ''
   const queryParams = ['foo', 'trois']
   const skipQueryParams = ['foo']
-  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, p, queryParams, skipQueryParams)
-  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, p, queryParams, skipQueryParams)
+  const name1 = mfn('http://example.com?foo=bar&trois=quatre&x=y', m, null, p, queryParams, skipQueryParams)
+  const name2 = mfn('http://example.com?trois=quatre&foo=bar', m, null, p, queryParams, skipQueryParams)
   expect(name1).toBe(name2)
 })
 
-it('Skipped post body params does not affects output name', () => {
+it('Skipped post body params for request with content-type="application/json" does not affects output name', () => {
   const m = 'POST'
   const postBody1 = JSON.stringify({ foo: 'bar', x: 2 })
   const postBody2 = JSON.stringify({ foo: 'bazzzz', x: 2 })
   const skipPostParams = ['foo']
-  const name1 = mfn('http://example.com', m, postBody1, [], [], skipPostParams)
-  const name2 = mfn('http://example.com', m, postBody2, [], [], skipPostParams)
+  const headers = {"content-type": "application/json"}
+  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
+  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
 
   expect(name1).toBe(name2)
 })
 
+it('Skipped post body params for request with content-type="application/x-www-form-urlencoded" does not affects output name', () => {
+  const m = 'POST'
+  const postBody1 = "foo=bar&x=2"
+  const postBody2 = "foo=bazzzz&x=2"
+  const skipPostParams = ['foo']
+  const headers = {"content-type": "application/x-www-form-urlencoded"}
+  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
+  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+
+  expect(name1).toBe(name2)
+})
+
+it('Skipped post body params for request without content-type affects output name', () => {
+  const m = 'POST'
+  const postBody1 = "foo=bar&x=2"
+  const postBody2 = "foo=bazzzz&x=2"
+  const skipPostParams = ['foo']
+  const headers = {}
+  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
+  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+
+  expect(name1).not.toBe(name2)
+})
+
+it('Skipped post body params for request with not supported content-type affects output name', () => {
+  const m = 'POST'
+  const postBody1 = "foo=bar&x=2"
+  const postBody2 = "foo=bazzzz&x=2"
+  const skipPostParams = ['foo']
+  const headers =  {"content-type": "multipart/form-data"}
+  const name1 = mfn('http://example.com', m, headers, postBody1, [], [], skipPostParams)
+  const name2 = mfn('http://example.com', m, headers, postBody2, [], [], skipPostParams)
+
+  expect(name1).not.toBe(name2)
+})
+
 it('Non-json post body does not throws an error', () => {
-  mfn('http://example.com', 'GET', 'post_body')
+  mfn('http://example.com', 'GET', null, 'post_body')
 })
